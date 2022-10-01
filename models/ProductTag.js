dd7abd26e4f7df8f63a14c03d7e670ee5409e04e
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-10-01 06:30:28 -0500

    add producttag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,17 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    // product_id: 1,
+    // tag_id: 8,
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      
+    }
   },
   {
     sequelize,
