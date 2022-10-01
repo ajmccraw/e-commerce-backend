71cec3aad74d75444b418b72cca10f6aef21c510
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-10-01 06:13:50 -0500

    input rest for product.js

--- models/Product.js
+++ models/Product.js
@@ -20,7 +20,26 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
-    
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
