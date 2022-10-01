738d0e4fefb3da6aba7b6423fda4dd6a2ecf9ff6
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-10-01 06:45:29 -0500

    tag, product, category index.js

--- models/index.js
+++ models/index.js
@@ -10,11 +10,23 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tagged_products',
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagged_products',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
